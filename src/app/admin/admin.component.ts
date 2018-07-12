28f0be9fdffab0a47b2cb1327262dcf8b194b775
johannesrehrl <41146719+johannesrehrl@users.noreply.github.com>
2018-07-12 14:23:12 +0200

    Admin area works, kinda.

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { LoginService } from '../login.service'
+import { LoginService } from '../login.service';
+import { ProductService } from '../product.service';
+import {Product} from '../product';
 
 @Component({
   selector: 'app-admin',
@@ -7,10 +9,51 @@ import { LoginService } from '../login.service'
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
+  private unique: boolean;
+  private deleted: boolean;
 
-  constructor(public loginService: LoginService) { }
+  constructor(public loginService: LoginService, public productService: ProductService) { }
 
   ngOnInit() {
   }
 
+  addProduct(name: string, price: number,): void {
+    this.unique = true;
+
+    for (let i = 0; i < this.productService.getProducts().length; i++) {
+      if (name === this.productService.getProducts()[i].name) {
+        this.unique = false;
+      }
+    }
+
+    if (name && price && this.unique) {
+      this.productService.getProducts().push(
+        new Product(this.productService.getProducts()[this.productService.getProducts().length - 1].id + 1, name, price)
+      );
+
+      alert('Product added!');
+    } else {
+      alert('Failed to create Product! Try again.');
+    }
+  }
+
+  deleteProduct(name: string, password: string): void {
+    this.deleted = false;
+
+    if (password === this.loginService.getCurrentUser().password) {
+      for (let i = 0; i < this.productService.getProducts().length; i++) {
+        if (this.productService.getProducts()[i].name === name) {
+          delete this.productService.getProducts()[i];
+          this.deleted = true;
+          alert('Product deleted!');
+        }
+      }
+    } else {
+      alert('Failed to delete Product! Try again.');
+    }
+
+    if (this.deleted === false) {
+      alert('Failed to delete Product! Try again.');
+    }
+  }
 }
